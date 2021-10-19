82016cf945292fc080313466f5ba32126e398e91
Shafiq Suhag <shafiqsuhag@gmail.com>
2021-10-19 23:21:55 +0600

    Final Update - 2

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -12,28 +12,13 @@ const Header = () => {
         <div>
             <Navbar  expand="lg">
                 <Container>
-                    <Link className=" fs-1 text-logo " to="/home">WSH</Link>
-                    {/* <Navbar.Brand as={Link} to="/home" className="fs-1 text-logo">BSH</Navbar.Brand> */}
+                    <Link className=" fs-1 text-logo " to="/home" title="World Specialized Hospital">WSH</Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mx-auto ">
                             <Link className=" navbar-link " to="/home">Home</Link>
                             <Link className=" navbar-link " to="/book-appointment">Book Appointment</Link>
                             <Link className=" navbar-link " to="/my-account">My Account </Link>
-                            {/* <Nav.Link  as={Link} to="/my-account" className=""> My Account </Nav.Link> */}
-                            
-                           
-                            {/* dropdropdown start  */}
-                             {/* <NavDropdown title="Dropdown3" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown> */}
-                            {/* dropdropdown end  */}
-                            
-                           
                         </Nav>
                         <Nav className="align-items-center">
                             {
